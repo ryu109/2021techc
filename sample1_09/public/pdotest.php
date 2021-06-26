337b2c383f3cef4db5d70a2fd8825ff9997024bd
MUTO Nao <n@o625.com>
2021-06-26 11:13:49 +0900

    MySQL立ち上げてPHPからINSERTするテスト

---
+++ sample1_09/public/pdotest.php
@@ -0,0 +1,8 @@
+<?
+$dbh = new PDO('mysql:host=mysql;dbname=techc', 'root', '');
+
+$insert_sth = $dbh->prepare("INSERT INTO hogehoge (text) VALUES (:text)");
+$insert_sth->execute(array(
+    ':text' => 'hello world!!!!!!!!!'
+));
+print('insertできました');
