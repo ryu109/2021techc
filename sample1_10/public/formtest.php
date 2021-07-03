b84222976868934e23016697c4f012938d6f7d5c
MUTO Nao <n@o625.com>
2021-07-03 11:25:52 +0900

    DBに保存してきたデータを表示

--- sample1_10/public/formtest.php
+++ sample1_10/public/formtest.php
@@ -17,6 +17,9 @@ if (isset($_POST['body'])) {
   return;
 }
 
+// いままで保存してきたものを取得
+$select_sth = $dbh->prepare('SELECT * FROM bbs_entries ORDER BY created_at DESC');
+$select_sth->execute();
 ?>
 
 <!-- フォームのPOST先はこのファイル自身にする -->
@@ -24,3 +27,14 @@ if (isset($_POST['body'])) {
   <textarea name="body"></textarea>
   <button type="submit">送信</button>
 </form>
+
+<hr>
+
+<?php foreach($select_sth as $entry): ?>
+  <dl style="margin-bottom: 1em; padding-bottom: 1em; border-bottom: 1px solid #ccc;">
+    <dt>日時</dt>
+    <dd><?= $entry['created_at'] ?></dd>
+    <dt>内容</dt>
+    <dd><?= nl2br(htmlspecialchars($entry['body'])) // 必ず htmlspecialchars() すること ?></dd>
+  </dl>
+<?php endforeach ?>
