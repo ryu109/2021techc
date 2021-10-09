e33c553ce311bff08f75e827825cd7fba94115dd
MUTO Nao <n@o625.com>
2021-10-09 17:43:00 +0900

    後期第2回 会員登録機能実装

---
+++ sample2_02/create_koki02_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `koki02_users` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `password` TEXT NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
+);
