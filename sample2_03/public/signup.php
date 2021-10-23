329555608db1a672ac20210b4b3df170dd780868
MUTO Nao <n@o625.com>
2021-10-23 09:28:40 +0900

    パスワードをsha256で暗号化して保存する

--- sample2_03/public/signup.php
+++ sample2_03/public/signup.php
@@ -23,7 +23,7 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password
   $insert_sth->execute([
       ':name' => $_POST['name'],
       ':email' => $_POST['email'],
-      ':password' => $_POST['password'],
+      ':password' => hash('sha256', $_POST['password']),
   ]); 
 
   // 処理が終わったら完了画面にリダイレクト
