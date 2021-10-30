dc34ffbfc09ce3a9bc57376e3228a8260992daff
MUTO Nao <n@o625.com>
2021-10-30 10:17:43 +0900

    セッションまわり使った機能をPHPの標準セッション機能に書き換え

--- sample2_04/public/login.php
+++ sample2_04/public/login.php
@@ -39,36 +39,10 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
     return;
   }
 
-
-  # ここからセッションの独自実装 (詳細は後期第1回授業参照) ##############
-  // セッションIDの取得(なければ新規で作成&設定)
-  $session_cookie_name = 'session_id';
-  $session_id = $_COOKIE[$session_cookie_name] ?? base64_encode(random_bytes(64));
-  if (!isset($_COOKIE[$session_cookie_name])) {
-    setcookie($session_cookie_name, $session_id);
-  }
-  
-  // 接続 (redisコンテナの6379番ポートに接続)
-  $redis = new Redis();
-  $redis->connect('redis', 6379);
-  
-  // Redisにセッション変数を保存しておくキー
-  $redis_session_key = "session-" . $session_id; 
-  
-  // Redisからセッションのデータを読み込み
-  // 既にセッション変数(の配列)が何かしら格納されていればそれを，なければ空の配列を $session_values変数に保存
-  $session_values = $redis->exists($redis_session_key)
-    ? json_decode($redis->get($redis_session_key), true) 
-    : []; 
-  
+  session_start();
   // セッションにログインできた会員情報の主キー(id)を設定
-  $session_values["login_user_id"] = $user['id'];
+  $_SESSION["login_user_id"] = $user['id'];
   
-  // セッションをRedisに保存
-  $redis->set($redis_session_key, json_encode($session_values));
-  # セッションここまで ###################################################
-
-
   // ログインが成功したらログイン完了画面にリダイレクト
   header("HTTP/1.1 302 Found");
   header("Location: ./login_finish.php");
