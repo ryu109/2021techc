a33e10742629bf0f040eafdabdcd590e672e97be
MUTO Nao <n@o625.com>
2021-11-21 09:32:09 +0900

    後期第8回授業用ディレクトリ作成

---
+++ sample2_08/public/profile.php
@@ -0,0 +1,83 @@
+<?php
+$user = null;
+if (!empty($_GET['user_id'])) {
+  $user_id = $_GET['user_id'];
+
+  // DBに接続
+  $dbh = new PDO('mysql:host=mysql;dbname=techc', 'root', '');
+  // 対象の会員情報を引く
+  $select_sth = $dbh->prepare("SELECT * FROM users WHERE id = :id");
+  $select_sth->execute([
+      ':id' => $user_id,
+  ]);
+  $user = $select_sth->fetch();
+}
+
+if (empty($user)) {
+  header("HTTP/1.1 404 Not Found");
+  print("そのようなユーザーIDの会員情報は存在しません");
+  return;
+}
+
+// 投稿データを取得。紐づく会員情報も結合し同時に取得する。
+$select_sth = $dbh->prepare(
+  'SELECT bbs_entries.*, users.name AS user_name, users.icon_filename AS user_icon_filename'
+  . ' FROM bbs_entries INNER JOIN users ON bbs_entries.user_id = users.id'
+  . ' WHERE user_id = :user_id'
+  . ' ORDER BY bbs_entries.created_at DESC'
+);
+$select_sth->execute([
+  ':user_id' => $user_id,
+]);
+?>
+<a href="/bbs.php">掲示板に戻る</a>
+
+<div style="
+    width: 100%; height: 15em;
+    <?php if(!empty($user['cover_filename'])): ?>
+    background: url('/image/<?= $user['cover_filename'] ?>') center;
+    <?php endif; ?>
+  ">
+</div>
+
+<h1><?= htmlspecialchars($user['name']) ?> さん のプロフィール</h1>
+
+<div>
+  <?php if(empty($user['icon_filename'])): ?>
+  現在未設定
+  <?php else: ?>
+  <img src="/image/<?= $user['icon_filename'] ?>"
+    style="height: 5em; width: 5em; border-radius: 50%; object-fit: cover;">
+  <?php endif; ?>
+</div>
+
+<?php if(!empty($user['birthday'])): ?>
+<?php
+  $birthday = DateTime::createFromFormat('Y-m-d', $user['birthday']);
+  $today = new DateTime('now');
+?>
+  <?= $today->diff($birthday)->y ?>歳
+<?php endif; ?>
+
+<div>
+  <?= nl2br(htmlspecialchars($user['introduction'])) ?>
+</div>
+
+<hr>
+
+
+<?php foreach($select_sth as $entry): ?>
+  <dl style="margin-bottom: 1em; padding-bottom: 1em; border-bottom: 1px solid #ccc;">
+    <dt>日時</dt>
+    <dd><?= $entry['created_at'] ?></dd>
+    <dt>内容</dt>
+    <dd>
+      <?= htmlspecialchars($entry['body']) ?>
+      <?php if(!empty($entry['image_filename'])): ?>
+      <div>
+        <img src="/image/<?= $entry['image_filename'] ?>" style="max-height: 10em;">
+      </div>
+      <?php endif; ?>
+    </dd>
+  </dl>
+<?php endforeach ?>
