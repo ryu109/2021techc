1ba62ebab9e1cb168135567c5161cc3135e33d2b
MUTO Nao <n@o625.com>
2021-12-05 07:20:47 +0900

    会員一覧画面作る

---
+++ sample2_10/public/users.php
@@ -0,0 +1,28 @@
+<?php
+
+$dbh = new PDO('mysql:host=mysql;dbname=techc', 'root', '');
+
+// 会員データを取得
+$select_sth = $dbh->prepare('SELECT * FROM users ORDER BY id DESC');
+$select_sth->execute();
+?>
+
+<body>
+  <h1>会員一覧</h1>
+
+  <?php foreach($select_sth as $user): ?>
+    <div style="display: flex; justify-content: start; align-items: center; padding: 1em 2em;">
+      <?php if(empty($user['icon_filename'])): ?>
+        <!-- アイコン無い場合は同じ大きさの空白を表示して揃えておく -->
+        <div style="height: 2em; width: 2em;"></div>
+      <?php else: ?>
+        <img src="/image/<?= $user['icon_filename'] ?>"
+          style="height: 2em; width: 2em; border-radius: 50%; object-fit: cover;">
+      <?php endif; ?>
+      <a href="/profile.php?user_id=<?= $user['id'] ?>" style="margin-left: 1em;">
+        <?= htmlspecialchars($user['name']) ?>
+      </a>
+    </div>
+    <hr style="border: none; border-bottom: 1px solid gray;">
+  <?php endforeach; ?>
+</body>
