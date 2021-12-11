a1b42b3b1bd33596001478dc47722488e9f4ee11
MUTO Nao <n@o625.com>
2021-12-11 09:42:51 +0900

    掲示板トップページから投稿機能削除 (タイムラインを主の画面とする)

--- sample2_10/public/bbs.php
+++ sample2_10/public/bbs.php
@@ -2,37 +2,6 @@
 $dbh = new PDO('mysql:host=mysql;dbname=techc', 'root', '');
 
 session_start();
-if (isset($_POST['body']) && !empty($_SESSION['login_user_id'])) {
-  // POSTで送られてくるフォームパラメータ body がある かつ ログイン状態 の場合
-
-  $image_filename = null;
-  if (!empty($_POST['image_base64'])) {
-    // 先頭の data:~base64, のところは削る
-    $base64 = preg_replace('/^data:.+base64,/', '', $_POST['image_base64']);
-
-    // base64からバイナリにデコードする
-    $image_binary = base64_decode($base64);
-
-    // 新しいファイル名を決めてバイナリを出力する
-    $image_filename = strval(time()) . bin2hex(random_bytes(25)) . '.png';
-    $filepath =  '/var/www/public/image/' . $image_filename;
-    file_put_contents($filepath, $image_binary);
-  }
-
-  // insertする
-  $insert_sth = $dbh->prepare("INSERT INTO bbs_entries (user_id, body, image_filename) VALUES (:user_id, :body, :image_filename)");
-  $insert_sth->execute([
-      ':user_id' => $_SESSION['login_user_id'], // ログインしている会員情報の主キー
-      ':body' => $_POST['body'], // フォームから送られてきた投稿本文
-      ':image_filename' => $image_filename, // 保存した画像の名前 (nullの場合もある)
-  ]);
-
-  // 処理が終わったらリダイレクトする
-  // リダイレクトしないと，リロード時にまた同じ内容でPOSTすることになる
-  header("HTTP/1.1 302 Found");
-  header("Location: ./bbs.php");
-  return;
-}
 
 // 投稿データを取得。紐づく会員情報も結合し同時に取得する。
 $select_sth = $dbh->prepare(
@@ -57,19 +26,9 @@ function bodyFilter (string $body): string
 ?>
 
 <?php if(empty($_SESSION['login_user_id'])): ?>
-  投稿するには<a href="/login.php">ログイン</a>が必要です。
+  <a href="/login.php">ログイン</a>して自分のタイムラインを閲覧しましょう！
 <?php else: ?>
-  現在ログイン中 (<a href="/setting/index.php">設定画面はこちら</a>)
-  <!-- フォームのPOST先はこのファイル自身にする -->
-  <form method="POST" action="./bbs.php"><!-- enctypeは外しておきましょう -->
-    <textarea name="body" required></textarea>
-    <div style="margin: 1em 0;">
-      <input type="file" accept="image/*" name="image" id="imageInput">
-    </div>
-    <input id="imageBase64Input" type="hidden" name="image_base64"><!-- base64を送る用のinput (非表示) -->
-    <canvas id="imageCanvas" style="display: none;"></canvas><!-- 画像縮小に使うcanvas (非表示) -->
-    <button type="submit">送信</button>
-  </form>
+  <a href="/timeline.php">タイムラインはこちら</a>
 <?php endif; ?>
 <hr>
 
@@ -108,54 +67,3 @@ function bodyFilter (string $body): string
     </dd>
   </dl>
 <?php endforeach ?>
-
-<script>
-document.addEventListener("DOMContentLoaded", () => {
-  const imageInput = document.getElementById("imageInput");
-  imageInput.addEventListener("change", () => {
-    if (imageInput.files.length < 1) {
-      // 未選択の場合
-      return;
-    }
-
-    const file = imageInput.files[0];
-    if (!file.type.startsWith('image/')){ // 画像でなければスキップ
-      return;
-    }
-
-    // 画像縮小処理
-    const imageBase64Input = document.getElementById("imageBase64Input"); // base64を送るようのinput
-    const canvas = document.getElementById("imageCanvas"); // 描画するcanvas
-    const reader = new FileReader();
-    const image = new Image();
-    reader.onload = () => { // ファイルの読み込み完了したら動く処理を指定
-      image.onload = () => { // 画像として読み込み完了したら動く処理を指定
-
-        // 元の縦横比を保ったまま縮小するサイズを決めてcanvasの縦横に指定する
-        const originalWidth = image.naturalWidth; // 元画像の横幅
-        const originalHeight = image.naturalHeight; // 元画像の高さ
-        const maxLength = 1000; // 横幅も高さも1000以下に縮小するものとする
-        if (originalWidth <= maxLength && originalHeight <= maxLength) { // どちらもmaxLength以下の場合そのまま
-            canvas.width = originalWidth;
-            canvas.height = originalHeight;
-        } else if (originalWidth > originalHeight) { // 横長画像の場合
-            canvas.width = maxLength;
-            canvas.height = maxLength * originalHeight / originalWidth;
-        } else { // 縦長画像の場合
-            canvas.width = maxLength * originalWidth / originalHeight;
-            canvas.height = maxLength;
-        }
-
-        // canvasに実際に画像を描画 (canvasはdisplay:noneで隠れているためわかりにくいが...)
-        const context = canvas.getContext("2d");
-        context.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-        // canvasの内容をbase64に変換しinputのvalueに設定
-        imageBase64Input.value = canvas.toDataURL();
-      };
-      image.src = reader.result;
-    };
-    reader.readAsDataURL(file);
-  });
-});
-</script>
