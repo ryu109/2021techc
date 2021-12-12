3685a350acc5d3b5658788fb79b008fbfa226b4f
MUTO Nao <n@o625.com>
2021-12-12 12:30:29 +0900

    タイムラインの描画をJSで行うようにする
    timeline_json.php から情報取得する

--- sample2_11/public/timeline.php
+++ sample2_11/public/timeline.php
@@ -44,33 +44,6 @@ if (isset($_POST['body']) && !empty($_SESSION['login_user_id'])) {
   header("Location: ./timeline.php");
   return;
 }
-
-// 投稿データを取得。IN句の中身もプレースホルダを使うために、$target_user_ids の要素数だけ「?」を付けている。
-$sql = 'SELECT bbs_entries.*, users.name AS user_name, users.icon_filename AS user_icon_filename'
-  . ' FROM bbs_entries'
-  . ' INNER JOIN users ON bbs_entries.user_id = users.id'
-  . ' WHERE'
-  . '   bbs_entries.user_id IN'
-  . '     (SELECT followee_user_id FROM user_relationships WHERE follower_user_id = :login_user_id)'
-  . '   OR bbs_entries.user_id = :login_user_id'
-  . ' ORDER BY bbs_entries.created_at DESC';
-$select_sth = $dbh->prepare($sql);
-$select_sth->execute([
-    ':login_user_id' => $_SESSION['login_user_id'],
-]);
-
-// bodyのHTMLを出力するための関数を用意する
-function bodyFilter (string $body): string
-{
-    $body = htmlspecialchars($body); // エスケープ処理
-    $body = nl2br($body); // 改行文字を<br>要素に変換
-
-    // >>1 といった文字列を該当番号の投稿へのページ内リンクとする (レスアンカー機能)
-    // 「>」(半角の大なり記号)は htmlspecialchars() でエスケープされているため注意
-    $body = preg_replace('/&gt;&gt;(\d+)/', '<a href="#entry$1">&gt;&gt;$1</a>', $body);
-
-    return $body;
-}
 ?>
 
 <div>
@@ -94,44 +67,64 @@ function bodyFilter (string $body): string
 </form>
 <hr>
 
-<?php foreach($select_sth as $entry): ?>
-  <dl style="margin-bottom: 1em; padding-bottom: 1em; border-bottom: 1px solid #ccc;">
-    <dt id="entry<?= htmlspecialchars($entry['id']) ?>">
-      番号
-    </dt>
-    <dd>
-      <?= htmlspecialchars($entry['id']) ?>
-    </dd>
-    <dt>
-      投稿者
-    </dt>
-    <dd>
-      <a href="/profile.php?user_id=<?= $entry['user_id'] ?>">
-        <?php if(!empty($entry['user_icon_filename'])): // アイコン画像がある場合は表示 ?>
-        <img src="/image/<?= $entry['user_icon_filename'] ?>"
-          style="height: 2em; width: 2em; border-radius: 50%; object-fit: cover;">
-        <?php endif; ?>
-
-        <?= htmlspecialchars($entry['user_name']) ?>
-        (ID: <?= htmlspecialchars($entry['user_id']) ?>)
-      </a>
-    </dd>
-    <dt>日時</dt>
-    <dd><?= $entry['created_at'] ?></dd>
-    <dt>内容</dt>
-    <dd>
-      <?= bodyFilter($entry['body']) ?>
-      <?php if(!empty($entry['image_filename'])): ?>
-      <div>
-        <img src="/image/<?= $entry['image_filename'] ?>" style="max-height: 10em;">
-      </div>
-      <?php endif; ?>
-    </dd>
-  </dl>
-<?php endforeach ?>
+<dl id="entryTemplate" style="display: none; margin-bottom: 1em; padding-bottom: 1em; border-bottom: 1px solid #ccc;">
+  <dt>番号</dt>
+  <dd data-role="entryIdArea"></dd>
+  <dt>投稿者</dt>
+  <dd>
+    <a href="" data-role="entryUserAnchor"></a>
+  </dd>
+  <dt>日時</dt>
+  <dd data-role="entryCreatedAtArea"></dd>
+  <dt>内容</dt>
+  <dd data-role="entryBodyArea">
+  </dd>
+</dl>
+<div id="entriesRenderArea"></div>
 
 <script>
 document.addEventListener("DOMContentLoaded", () => {
+  const entryTemplate = document.getElementById('entryTemplate');
+  const entriesRenderArea = document.getElementById('entriesRenderArea');
+
+  const request = new XMLHttpRequest();
+  request.onload = (event) => {
+    const response = event.target.response;
+    response.entries.forEach((entry) => {
+      // テンプレートとするものから要素をコピー
+      const entryCopied = entryTemplate.cloneNode(true);
+
+      // display: none を display: block に書き換える
+      entryCopied.style.display = 'block';
+
+      // id属性を設定しておく(レスアンカ用)
+      entryCopied.id = 'entry' + entry.id.toString();
+
+      // 番号(ID)を表示
+      entryCopied.querySelector('[data-role="entryIdArea"]').innerText = entry.id.toString();
+
+      // 名前を表示
+      entryCopied.querySelector('[data-role="entryUserAnchor"]').innerText = entry.user_name;
+
+      // 名前のところのリンク先(プロフィール)のURLを設定
+      entryCopied.querySelector('[data-role="entryUserAnchor"]').href = entry.user_profile_url;
+
+      // 投稿日時を表示
+      entryCopied.querySelector('[data-role="entryCreatedAtArea"]').innerText = entry.created_at;
+
+      // 本文を表示 (ここはHTMLなのでinnerHTMLで)
+      entryCopied.querySelector('[data-role="entryBodyArea"]').innerHTML = entry.body;
+
+      // 最後に実際の描画を行う
+      entriesRenderArea.appendChild(entryCopied);
+    });
+  }
+  request.open('GET', '/timeline_json.php', true); // timeline_json.php を叩く
+  request.responseType = 'json';
+  request.send();
+
+
+  // 以下画像縮小用
   const imageInput = document.getElementById("imageInput");
   imageInput.addEventListener("change", () => {
     if (imageInput.files.length < 1) {
